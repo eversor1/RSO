b9910c16e9c1cece589bf9ed483b32fdc3437f59
eversor1 <jshipp@polywatt.org>
2016-04-04 22:31:39 -0400

    small updates

--- RSO/Service1.cs
+++ RSO/Service1.cs
@@ -26,7 +26,7 @@ namespace RSO
             Newtonsoft.Json.Linq.JObject config = Newtonsoft.Json.Linq.JObject.Parse(ServicesOrder);
             //ServiceOperations ServicesFile = JsonConvert.DeserializeObject<configSchema>(ServicesOrder);
             string output = JsonConvert.SerializeObject(config);
-            System.IO.File.Create(AppDomain.CurrentDomain.BaseDirectory + "OnStart.txt");
+            //System.IO.File.Create(AppDomain.CurrentDomain.BaseDirectory + "OnStart.txt");
             File.WriteAllText(AppDomain.CurrentDomain.BaseDirectory + "OnStart.txt", (string)config);
         }
 
